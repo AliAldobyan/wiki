e54e912f14324517576c1ec317f1893e26534206
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-08-31 11:31:40 +0300

    Django, wiki mini project is completed and working

--- pages/models.py
+++ pages/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class Page(models.Model):
 	title = models.CharField(max_length=120)
 	content = models.TextField()
-	last_update = models.DateField(auto_now_add=True)
+	last_update = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.title
@@ -13,4 +13,4 @@ class Page(models.Model):
 
 	def get_absolute_url(self):
 
-		return reverse('page-detail' , args=[self.id])
+		return reverse('page-detail' , kwargs={"page_id" : self.id})
